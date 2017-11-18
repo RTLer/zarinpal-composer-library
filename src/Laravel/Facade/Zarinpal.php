d5717bfdee666a68add165524047ec3019e4fdeb
hooman naghiee <RTLer@users.noreply.github.com>
2017-11-18 23:21:14 +0330

    add php documents to laravel Facade

--- src/Laravel/Facade/Zarinpal.php
+++ src/Laravel/Facade/Zarinpal.php
@@ -4,6 +4,12 @@ namespace Zarinpal\Laravel\Facade;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static request($callbackURL, $Amount, $Description, $Email = null, $Mobile = null)
+ * @method static verify($status, $amount, $authority)
+ * @method static redirect()
+ * @method static getDriver()
+ */
 class Zarinpal extends Facade
 {
     /**
